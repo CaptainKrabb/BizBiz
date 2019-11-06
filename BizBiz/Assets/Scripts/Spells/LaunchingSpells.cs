5f10d6a8dc1f26bbd07ce284e27b07d41251bda6
FernandesWilliam <57043613+FernandesWilliam@users.noreply.github.com>
2019-11-06 10:09:19 +0100

    Init

--- BizBiz/Assets/Scripts/Spells/LaunchingSpells.cs
+++ BizBiz/Assets/Scripts/Spells/LaunchingSpells.cs
@@ -9,27 +9,6 @@ public class LaunchingSpells : MonoBehaviour
         turnResp = GameObject.Find("TurnResp").GetComponent<TurnResp>() as TurnResp;
     }
     public void spell1() {
-        Character chara = turnResp.Characters[turnResp.IndexTurn];
-        if (chara.gameObject.tag == "Player") {
-            chara.IsUsingSpell = 0;
-        }
-    }
-    public void spell2() {
-        Character chara = turnResp.Characters[turnResp.IndexTurn];
-        if (chara.gameObject.tag == "Player") {
-            chara.IsUsingSpell = 1;
-        }
-    }
-    public void spell3() {
-        Character chara = turnResp.Characters[turnResp.IndexTurn];
-        if (chara.gameObject.tag == "Player") {
-            chara.IsUsingSpell = 2;
-        }
-    }
-    public void spell4() {
-        Character chara = turnResp.Characters[turnResp.IndexTurn];
-        if (chara.gameObject.tag == "Player") {
-            chara.IsUsingSpell = 3;
-        }
+        turnResp.Characters[turnResp.IndexTurn].isUsingSpell = 1;
     }
 }
